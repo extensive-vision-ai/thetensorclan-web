afc6c1865ed7092898be309e7ea2b3742343e8c4
Satyajit Ghana <satyajitghana7@gmail.com>
2020-10-11 14:39:35 +0530

    fix size of logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,6 @@ import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 
 const TitleWrapper = styled(Link)`
-    font-size: calc(5vh + 1vw);
-    max-width: 100%;
-    font-weight: bolder;
-    color: #000;
-    text-decoration: none;
-    text-shadow: 4px 3px 4px #8395a7;
-
-    &:focus,
-    &:hover,
-    &:visited,
-    &:link,
-    &:active {
-        text-decoration: none;
-    }
-`;
-
-const LogoImage = styled(Image)`
     width: 80%;
     /* Medium devices (tablets, 768px and up) */
     @media (min-width: 768px) and (max-width: 991.98px) {
@@ -39,15 +22,31 @@ const LogoImage = styled(Image)`
     }
 `;
 
+const LogoImage = styled(Image)`
+    width: 80%;
+    /* Medium devices (tablets, 768px and up) */
+    @media (min-width: 768px) and (max-width: 991.98px) {
+        width: 80%;
+    }
+
+    /* Small devices (landscape phones, 576px and up)*/
+    @media (min-width: 576px) and (max-width: 767.98px) {
+        width: 110%;
+    }
+
+    /* Extra small devices (portrait phones, less than 576px) */
+    @media (max-width: 575.98px) {
+        width: 110%;
+    }
+`;
+
 const Header = ({ title }) => {
     return (
         <Container>
             <Navbar variant="light" bg="none" sticky="top" className="px-2">
-                {/* <TitleWrapper className="display-4 m-auto py-3" to="/">
-                    <i className="fas fa-quidditch" /> {title}
-                </TitleWrapper> */}
-                {/* <Image src={logo}/> */}
-                <LogoImage src={logo} className="mx-auto" />
+                <TitleWrapper to="/" className="mx-auto">
+                    <LogoImage src={logo} className="mx-auto" />
+                </TitleWrapper>
             </Navbar>
             <hr />
         </Container>
